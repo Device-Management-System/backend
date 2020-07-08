bbb4fe372e6ed16297d6c5d7de838f0891f7aa67
Marina Baskova <marinabaskova24@gmail.com>
2020-07-08 15:52:11 -0700

    users migration

---
+++ migrations/20200708150624_users.js
@@ -0,0 +1,15 @@
+exports.up = function (knex) {
+  return knex.schema.createTable('users', (tbl) => {
+    tbl.increments().primary();
+    tbl.varchar('first_name', 30).notNullable();
+    tbl.varchar('last_name', 30).notNullable();
+    tbl.varchar('email', 100).notNullable();
+    tbl.integer('uuid').notNullable();
+    tbl.varchar('role', 100).notNullable();
+    tbl.boolean('is_employed', 30).notNullable();
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists('users');
+};
