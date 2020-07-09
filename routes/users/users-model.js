4bd1fe4ab2e5deda83b29cc3e7e9cc30bb445cfb
Marina Baskova <marinabaskova24@gmail.com>
2020-07-09 15:15:30 -0700

    setup users model

--- routes/users/users-model.js
+++ routes/users/users-model.js
@@ -2,9 +2,50 @@ const db = require('../../data/dbConfig');
 
 module.exports = {
   findAll,
+  findById,
+  findByEmail,
+  add,
+  update,
+  remove,
 };
 
 async function findAll() {
   const allUsers = await db('users');
   return allUsers;
+}
+
+async function findById(id) {
+  const user = await db('users').where({ id }).first();
+  return user;
+}
+
+async function findByEmail(email) {
+  const user = await db('users').where({ email }).first();
+  return user;
+}
+
+async function add(newUser) {
+  const [id] = await db('users').insert(newUser, 'id');
+  if (id) {
+    const user = await findById(id);
+    return user;
+  }
+}
+
+async function update(infoToUpdate, id) {
+  const editedUser = await db('users').where({ id }).update(infoToUpdate);
+  if (editedUser) {
+    const updatedUser = await findById(id);
+    return updatedUser;
+  }
+}
+
+async function remove(id) {
+  const userToDelete = await findById(id);
+  if (userToDelete) {
+    const numOfDeletedRecords = await db('users').where({ id }).del();
+    if (numOfDeletedRecords) {
+      return userToDelete;
+    }
+  }
 }
