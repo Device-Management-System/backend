c1f9aab545103affd5db6b0a7f36812d570a88a8
guillsav <guillsav@gmail.com>
2020-07-08 21:14:49 -0400

    Added .prettierrc file

--- api/server.js
+++ api/server.js
@@ -1,13 +1,13 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
 
 const server = express();
 
 server.use(express.json());
 server.use(cors());
 
-server.get("/", async (req, res) => {
-  res.status(200).json({ api: "server is working" });
+server.get('/', async (req, res) => {
+  res.status(200).json({ api: 'server is working' });
 });
 
 module.exports = server;
