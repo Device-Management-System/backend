5a58b88734ce8f5eed3d75191be24ab306283e59
guillsav <guillsav@gmail.com>
2020-07-09 15:26:06 -0400

    remove console.log from devices route

--- routes/devices/devices-route.js
+++ routes/devices/devices-route.js
@@ -10,7 +10,6 @@ router.post('/', async (req, res) => {
     const device = await db.add(req.body);
     res.status(200).json(device);
   } catch ({ message }) {
-    console.log(message);
     res.status(500).json({ message: 'Unable to create device the device' });
   }
 });
