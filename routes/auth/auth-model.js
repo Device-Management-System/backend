6f3397aca18891ef1890c0b1f2719e8826f4e177
tjforeman <tylerforeman92@gmail.com>
2020-07-12 23:27:48 -0400

    changed syntax on auth-model

--- routes/auth/auth-model.js
+++ routes/auth/auth-model.js
@@ -1,16 +1,16 @@
 const db = require('../../data/dbConfig');
 
+const findUserByEmail = async (userEmail) => {
+  const user = await db('users').where({ email: userEmail });
+  return user;
+};
+
+const addUser = async (userInfo) => {
+  const newUser = await db('users').insert(userInfo);
+  return newUser;
+};
+
 module.exports = {
   findUserByEmail,
   addUser,
 };
-
-async function findUserByEmail(userEmail) {
-  const user = await db('users').where({ email: userEmail });
-  return user;
-}
-
-async function addUser(userInfo) {
-  const newUser = await db('users').insert(userInfo);
-  return newUser;
-}
