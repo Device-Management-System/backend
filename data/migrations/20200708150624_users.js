79ba9b2f4ab1fdeb2d27a2b5a1587dab876d0557
guillsav <guillsav@gmail.com>
2020-07-21 01:47:45 -0400

    Added register validation and updated auth route

--- data/migrations/20200708150624_users.js
+++ data/migrations/20200708150624_users.js
@@ -3,7 +3,7 @@ exports.up = function (knex) {
     tbl.increments().primary();
     tbl.varchar('name', 30).notNullable();
     tbl.varchar('email', 100).notNullable().unique();
-    tbl.text('uuid').notNullable();
+    tbl.text('uuid').notNullable().unique();
     tbl.varchar('role', 100);
     tbl.boolean('is_employed', 30).defaultTo(true);
     tbl.boolean('is_admin', 30).defaultTo(false);
